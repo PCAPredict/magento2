43785a75f68af9285587dba3b4769dc73f6152c9
Matthew Garde <matthewgarde@pcapredict.com>
2016-12-19 12:01:28 +0000

    Separated out the selected countries from magento into a class to call on instead for init and reset.

---
+++ Package-Me.bat
@@ -0,0 +1,39 @@
+
+@REM Get current working dir name to use as archive name.
+@for %%a in (.) do @set currentfolder=%%~na
+@SET @CURR_DIR=%currentfolder%
+
+@REM Try to delete the file only if it exists
+@IF EXIST %@CURR_DIR%.tgz GOTO DELETE
+@echo No archive with name '%@CURR_DIR%.tgz' currently exists, will create new archive.
+@GOTO CREATE 
+
+:DELETE
+@del /F %@CURR_DIR%.tgz
+@printf \n
+@echo Deleted old archive.
+
+@REM If the file wasn't deleted for some reason, stop and error
+@IF EXIST %@CURR_DIR% GOTO ERROR
+
+:CREATE
+@REM If the file wasn't deleted for some reason, stop and error otherwise archive
+@IF NOT EXIST "C:\Program Files\7-Zip" GOTO NO7ZIP
+@"C:\Program Files\7-Zip\7z.exe" a -tzip %@CURR_DIR%.zip * -xr0!*.bat
+@GOTO SUCCESS
+
+:ERROR 
+@printf \n
+@echo %@CURR_DIR%.tgz could not be created, existing one could not be deleted.
+@GOTO FINISH
+
+:NO7ZIP
+@printf \n
+@echo 7-zip could not be found.
+@GOTO FINISH
+
+:SUCCESS
+@printf \n
+@echo %@CURR_DIR%.tgz Created Successfully.
+
+:FINISH
--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -5,25 +5,21 @@ class InstallData implements \Magento\Framework\Setup\InstallDataInterface
 {
     protected $settingsDataFactory;
 
-    /**
-     * Init
-     *
-     * @param AuthorizationFactory $authFactory
-     */
-    public function __construct(\PCAPredict\Tag\Model\SettingsDataFactory     $settingsDataFactory)
+    protected $settingsHelper;
+    
+    public function __construct(\PCAPredict\Tag\Model\SettingsDataFactory $settingsDataFactory, \PCAPredict\Tag\Helper\SettingsData $settingsHelper)
     {
         $this->settingsDataFactory = $settingsDataFactory;
+        $this->settingsHelper = $settingsHelper;
     }
     
     public function install(\Magento\Framework\Setup\ModuleDataSetupInterface $setup, \Magento\Framework\Setup\ModuleContextInterface $context)
     {
-        $settingsHelper = $this->helper('PCAPredict\Tag\Helper\SettingsData');
-
         $settings = $this->settingsDataFactory->create();
 
         $settings->setAccountCode('');
         $settings->setAccountToken('');
-        $settings->setfieldMappings($settingsHelper->getDefaultMappings());
+        $settings->setfieldMappings($this->settingsHelper->getDefaultMappings());
         $settings->setCustomJavascript('');
 
         $settings->save();
