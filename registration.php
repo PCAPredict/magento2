e16985b5b42024c35205e57f99ee5baff7d118d7
Matthew Garde <matthewgarde@pcapredict.com>
2016-12-20 11:30:24 +0000

    Pulling in branch, clear this one out first to help with merge conflicts.

--- registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © 2016 PCA Predict. All rights reserved.
- */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'PCAPredict_Tag',
-    __DIR__
-);
